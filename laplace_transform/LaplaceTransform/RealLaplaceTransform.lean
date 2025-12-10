94a2bba9e62e8d76b435a267e623de0c35744af0
antoine-vinciguerra <antoine.v@campus.technion.ac.il>
2025-12-10 15:51:18 +0200

    table Laplace 2

--- laplace_transform/LaplaceTransform/RealLaplaceTransform.lean
+++ laplace_transform/LaplaceTransform/RealLaplaceTransform.lean
@@ -24,7 +24,7 @@ import LaplaceTransform.LaplaceTransformDef
 import Mathlib.MeasureTheory.Measure.Complex
 
 import Mathlib.Data.Complex.Basic
-
+import Mathlib.Data.List.Defs
 
 
 
@@ -116,4 +116,133 @@ theorem RealLaplaceTransform_additive
     simp_all only [Pi.add_apply, f_tilde₁, f_tilde₂]
     exact h_int₂
   apply GeneralizedLaplaceTransform_additive L f_tilde₁ f_tilde₂ μ_c p h_integrable₁ h_integrable₂
+
 end Defs
+
+section LaplaceTable
+
+open Complex
+
+/--
+In this section, we will define tables of Laplace transform.
+The table will consist of pairs LaplacePair :
+1.a name of the function: no equality of functions is known apparently in Lean
+2. the laplace transform
+3. the values z for which the Laplace Transform converges
+For now we will do the convergence check manually
+-/
+structure LaplacePair where
+  name : String
+  original_function : ℝ → ℂ
+  laplace_transform : ℂ → ℂ
+  convergence_set: Set ℂ
+
+
+namespace LaplaceDB
+
+open LaplacePair
+open Complex
+
+/--
+define the Laplace Table
+--/
+abbrev Table := List LaplacePair
+def UsualLaplaceTable : Table := []
+
+
+
+/--
+The next function is to update the table.
+The function takes as entry:
+1. the Laplace table
+2. the function we want to add/update and its name
+3. a convergence set
+4. a proof that for all s∈E , the laplace transform is well defined.
+
+--/
+
+def update_laplace_table (table : Table) (f_name:String) (f : ℝ → ℂ) (E : Set ℂ)
+    (h_integrable : ∀ s ∈ E, Integrable (RealFullLaplaceKernel f s) μ_c) :
+    Table :=
+    match table with
+      |[] =>
+        [{
+      name := f_name,
+      original_function := f,
+      laplace_transform := RealLaplaceTransform f,
+      convergence_set := E
+        }]
+
+      | p :: rest =>
+        if p.name = f_name then
+          {
+        name := f_name,
+        original_function := f,
+        laplace_transform := RealLaplaceTransform f,
+        convergence_set := p.convergence_set ∪ E
+          } :: rest
+        else
+          p :: update_laplace_table rest f_name f E h_integrable
+
+/--
+Here we give a function version of the Laplace Transform
+--/
+def update_laplace_table_with_transform (table : Table)(f_name:String) (f : ℝ → ℂ) (g : ℂ → ℂ) (E : Set ℂ)
+  (h_integrable : ∀ s ∈ E, Integrable (RealFullLaplaceKernel f s) μ_c)
+  (h_g_is_transform: ∀ s ∈ E, RealLaplaceTransform f s = g s):
+  Table :=
+    match table with
+      |[] =>
+        [{
+      name := f_name,
+      original_function := f,
+      laplace_transform := g,
+      convergence_set := E
+        }]
+
+      | p :: rest =>
+        if p.name = f_name then
+          {
+        name := f_name,
+        original_function := f,
+        laplace_transform := g,
+        convergence_set := p.convergence_set ∪ E
+          } :: rest
+        else
+          p :: (update_laplace_table_with_transform rest f_name f g E h_integrable h_g_is_transform)
+
+/--
+These functions look for a Laplace Pair and either output:
+ the laplace pair
+ the Laplace Transform
+ the space of convergence
+--/
+def find_laplace_pair.LaplacePair(table : Table)(f_name:String):Option LaplacePair:=
+  match table with
+    |[] =>
+      none
+    | p :: rest =>
+        if p.name = f_name then
+          p
+        else
+         find_laplace_pair.LaplacePair rest f_name
+
+def find_laplace_pair.LaplaceTrans(table : Table)(f_name:String):Option (ℂ → ℂ):=
+  match table with
+    |[] =>
+      none
+    | p :: rest =>
+      if p.name = f_name then
+        p.laplace_transform
+      else
+        find_laplace_pair.LaplaceTrans rest f_name
+
+def find_laplace_pair.ConvSet(table : Table)(f_name:String):Option (Set ℂ):=
+  match table with
+    |[] =>
+      none
+    | p :: rest =>
+      if p.name = f_name then
+        p.convergence_set
+      else
+        find_laplace_pair.ConvSet rest f_name
