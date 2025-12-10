456205975fafbd4e3e5c73bd5bb42068872ea97e
theIceberg <daniel.goldberg.w@gmail.com>
2025-12-10 18:56:53 +0200

    started real laplace with formula in R

--- laplace_transform/LaplaceTransform/RealLaplaceTransform.lean
+++ laplace_transform/LaplaceTransform/RealLaplaceTransform.lean
@@ -10,6 +10,10 @@ import Mathlib.MeasureTheory.Integral.Lebesgue.Countable
 import Mathlib.Dynamics.Ergodic.MeasurePreserving
 import Mathlib.MeasureTheory.Integral.Lebesgue.Basic
 import Mathlib.MeasureTheory.Integral.Lebesgue.Norm
+import Mathlib.MeasureTheory.Measure.Lebesgue.Complex
+import Mathlib.MeasureTheory.Integral.Prod
+
+
 
 import Mathlib.Topology.Instances.Complex
 import Mathlib.Analysis.SpecialFunctions.Exponential
@@ -66,13 +70,13 @@ def μ_c: Measure ℂ := volume.restrict half_plane_pos_re
 
 def RealFullLaplaceKernel (f :ℝ → ℂ) (s : ℂ) : ℂ→ ℂ :=
   let f_tilde (z : ℂ) : ℂ :=
-    (Complex.exp ((-z.im)^2)) *f z.re / (Real.sqrt Real.pi)
+    (Complex.exp (-(z.im)^2)) *f z.re / (Real.sqrt Real.pi)
   fullLaplaceKernel L f_tilde s
 
 
 def RealLaplaceTransform (f :ℝ → ℂ) : ℂ → ℂ  :=
   let f_tilde (z : ℂ) : ℂ :=
-    (Complex.exp ((-z.im)^2)) *f z.re / (Real.sqrt Real.pi)
+    (Complex.exp (-(z.im)^2)) *f z.re / (Real.sqrt Real.pi)
   GeneralizedLaplaceTransform L f_tilde μ_c
 
 theorem RealLaplaceTransform_const_smul
@@ -81,21 +85,21 @@ theorem RealLaplaceTransform_const_smul
   RealLaplaceTransform  (r • f) p = r • RealLaplaceTransform f p := by
   unfold RealLaplaceTransform
   let f_tilde (z : ℂ) : ℂ :=
-    (Complex.exp ((-z.im)^2)) *f z.re / (Real.sqrt Real.pi)
+    (Complex.exp (-z.im)^2) *f z.re / (Real.sqrt Real.pi)
   have h_rf_tilde:
-  (fun z ↦ (Complex.exp ((-z.im)^2)) *( (r • f) z.re)
+  (fun z ↦ Complex.exp (-z.im) ^ 2 * (r • f) z.re
     / Real.sqrt Real.pi)= r •f_tilde:= by
-      ext z
-      simp[f_tilde]
-      have h_rf_tilde_z:
-      cexp (↑z.im ^ 2) * (r * f z.re) / ↑√Real.pi
-      = r * (cexp (↑z.im ^ 2) * f z.re / ↑√Real.pi):= by calc
-        cexp (↑z.im ^ 2) * (r * f z.re) / ↑√Real.pi=
-        (cexp (↑z.im ^ 2) * r * f z.re) / ↑√Real.pi:= by rw [@NonUnitalRing.mul_assoc]
-        _=(r* cexp (↑z.im ^ 2) * f z.re) / ↑√Real.pi:= by rw [mul_comm (cexp (↑z.im ^ 2)) r]
-        _=r* (cexp (↑z.im ^ 2) * f z.re) / ↑√Real.pi:= by rw [@NonUnitalRing.mul_assoc]
-        _=r * (cexp (↑z.im ^ 2) * f z.re / ↑√Real.pi):= by rw [@mul_div_assoc]
-      rw[h_rf_tilde_z]
+    ext x
+    simp[f_tilde]
+    have h_rf_tilde_x:
+    cexp (-↑x.im) ^ 2 * (r * f x.re) / ↑√Real.pi = r * (cexp (-↑x.im) ^ 2 * f x.re / ↑√Real.pi)
+    := by calc
+    cexp (-↑x.im) ^ 2 * (r • f) x.re / ↑√Real.pi=  r •(cexp (-↑x.im) ^ 2 *f x.re/ ↑√Real.pi):= by
+      simp only [Pi.smul_apply, smul_eq_mul]
+
+
+    _= (r • f_tilde) x:= by rw [@Pi.smul_apply]
+
   rw[h_rf_tilde]
   have h_integrable: Integrable (fullLaplaceKernel L f_tilde p) μ_c:= by
     simp_all only [Pi.smul_apply, smul_eq_mul, f_tilde]
@@ -113,28 +117,16 @@ theorem RealLaplaceTransform_additive
   RealLaplaceTransform (f₁ + f₂) p =  RealLaplaceTransform f₁ p + RealLaplaceTransform f₂ p := by
   unfold RealLaplaceTransform
   let f_tilde₁ (z : ℂ) : ℂ :=
-    (Complex.exp ((-z.im)^2)) *f₁ z.re / (Real.sqrt Real.pi)
+      if z.im = 0 then f₁ z.re else 0
   let f_tilde₂ (z : ℂ) : ℂ :=
-    (Complex.exp ((-z.im)^2)) *f₂ z.re / (Real.sqrt Real.pi)
-
-  have f_tilde_linear: (fun z ↦ cexp ((-↑z.im) ^ 2) * (f₁ + f₂) z.re / ↑√Real.pi)= f_tilde₁+ f_tilde₂:= by
+      if z.im = 0 then f₂ z.re else 0
+  have f_tilde_linear: (fun z ↦ if z.im = 0 then (f₁ + f₂) z.re else 0)= f_tilde₁+ f_tilde₂:= by
     ext z
-    simp[f_tilde₁, f_tilde₂]
-    have  f_tilde_linear_z:
-    cexp (↑z.im ^ 2) * (f₁ z.re + f₂ z.re) / ↑√Real.pi =
-  cexp (↑z.im ^ 2) * f₁ z.re / ↑√Real.pi
-  + cexp (↑z.im ^ 2) * f₂ z.re / ↑√Real.pi := by calc
-    cexp (↑z.im ^ 2) * (f₁ z.re + f₂ z.re) / ↑√Real.pi=
-    (cexp (↑z.im ^ 2) * (f₁ z.re + f₂ z.re)) / ↑√Real.pi:= by rw [div_ofReal]
-      _= (cexp (↑z.im ^ 2) * f₁ z.re + cexp (↑z.im ^ 2) * f₂ z.re) / ↑√Real.pi:= by rw [@left_distrib]
-      _= (cexp (↑z.im ^ 2) * f₁ z.re + cexp (↑z.im ^ 2) * f₂ z.re) *(1/ ↑√Real.pi):= by rw [@mul_one_div]
-      _= (cexp (↑z.im ^ 2) * f₁ z.re )*(1/ ↑√Real.pi)+ ( cexp (↑z.im ^ 2) * f₂ z.re) *(1/ ↑√Real.pi):= by rw [@NonUnitalNonAssocRing.right_distrib]
-      _= (cexp (↑z.im ^ 2) * f₁ z.re )/↑√Real.pi+  (cexp (↑z.im ^ 2) * f₂ z.re)*(1/ ↑√Real.pi):= by
-        congr 1
-        rw [@mul_one_div]
-      _= (cexp (↑z.im ^ 2) * f₁ z.re )/↑√Real.pi+  (cexp (↑z.im ^ 2) * f₂ z.re)/↑√Real.pi:= by
-        rw [@mul_one_div]
-    rw[f_tilde_linear_z]
+    simp only [Pi.add_apply]
+    unfold f_tilde₁ f_tilde₂
+    split
+    next h => simp_all only
+    next h => simp_all only [add_zero]
   rw[f_tilde_linear]
   have h_integrable₁: Integrable (fullLaplaceKernel L f_tilde₁ p) μ_c:= by
     simp_all only [Pi.add_apply, f_tilde₁, f_tilde₂]
@@ -148,16 +140,19 @@ theorem RealLaplaceTransform_additive
 theorem RealLaplaceTransformIs
    (f: ℝ → ℂ) (p: ℂ):
   RealLaplaceTransform f p = ∫t, NormedSpace.exp ℂ (-p*t) * (f t) ∂μ  := by
-  let f_tilde (z : ℂ) : ℂ :=
-      if z.im = 0 then f z.re else 0
+  let all_R: set ℝ := ℝ
+  let μ_2 : Measure ℝ × ℝ := volume.restrict (non_negative_reals × ℝ )
   unfold RealLaplaceTransform
   unfold L
   unfold GeneralizedLaplaceTransform
   unfold fullLaplaceKernel
   unfold laplaceKernel
   simp_all only [smul_eq_mul, mul_one, ite_mul, zero_mul, neg_mul]
-
-
+  have equiv := Complex.measurableEquivRealProd
+  have  equality_laplace:∫ (e : ℂ), cexp (-↑e.im ^ 2) * f e.re / ↑√Real.pi * NormedSpace.exp ℂ (-(e * p)) ∂μ_c =
+  ∫ (t : ℝ), NormedSpace.exp ℂ (-(p * ↑t)) * f t ∂μ := by calc
+    ∫ (e : ℂ), cexp (-↑e.im ^ 2) * f e.re / ↑√Real.pi * NormedSpace.exp ℂ (-(e * p)) ∂μ_c =
+    ∫ (e: ( ℝ× ℝ), cexp (-e.2 ^ 2) * f e.1 / ↑√Real.pi * NormedSpace.exp ℂ (-(e.1 * p)) ∂μ_c
 
 def g (t : ℝ) : ℂ := 1
 
